064cf7962641b9f32c6efc8d0df20190c77813df
Per Bergland <per@refapp.se>
2023-03-22 12:36:24 +0100

    select config file

--- react-sample/src/App.tsx
+++ react-sample/src/App.tsx
@@ -1,25 +1,54 @@
+import { Box, MenuItem, Select, Typography } from "@mui/material";
 import * as React from "react";
 import { AtsConfigPreview } from "./AtsConfigPreview";
 import { RefappAtsConfig } from "./lib/ats-types";
 import "./style.css";
 
+/**
+ * From the config-examples directory in this repository
+ */
+const configFiles = [
+  "Cost Centers, existing project, Swedish.json",
+  "Cost Centers.json",
+  "External Recruitment, pre-filled values from default project template.json",
+  "Project Templates, existing project, Swedish.json",
+  "Project Templates.json",
+] as const;
+
 export default function App() {
+  const [atsConfigFile, setAtsConfigFile] = React.useState<string>();
   const [atsConfig, setAtsConfig] = React.useState<RefappAtsConfig>();
 
   React.useEffect(() => {
+    if (!atsConfigFile) {
+      return;
+    }
     fetch(
-      "https://raw.githubusercontent.com/ref-app/refapp-api/main/config-examples/Cost%20Centers%2C%20existing%20project%2C%20Swedish.json"
+      `https://raw.githubusercontent.com/ref-app/refapp-api/main/config-examples/${encodeURIComponent(
+        atsConfigFile
+      )}`
     ).then((response) => {
       response.json().then((config) => {
         setAtsConfig(config);
       });
     });
-  });
+  }, [atsConfigFile]);
 
   return (
-    <div>
-      <h1>Refapp configuration UI sample</h1>
+    <Box display="flex" flexDirection="column" gap={2}>
+      <Typography variant="h1">Refapp configuration UI sample</Typography>
+      <Select
+        value={atsConfigFile}
+        onChange={(e) => setAtsConfigFile(e.target.value)}
+      >
+        {configFiles.map((f) => (
+          <MenuItem key={f} value={f}>
+            {f}
+          </MenuItem>
+        ))}
+      </Select>
+
       {atsConfig && <AtsConfigPreview configFields={atsConfig.config.fields} />}
-    </div>
+    </Box>
   );
 }
