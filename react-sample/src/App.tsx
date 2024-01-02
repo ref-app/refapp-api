bea333277dca24aaf4735a9000984dca418f25fb
Martijn van der Ven <martijn.vanderven@refapp.com>
2024-01-02 16:13:00 +0100

    First working version

--- react-sample/src/App.tsx
+++ react-sample/src/App.tsx
@@ -1,9 +1,20 @@
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Theme } from "@emotion/react";
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
   createTheme,
+  CssBaseline,
   MenuItem,
   Select,
+  TextField,
   ThemeProvider,
   Typography,
 } from "@mui/material";
@@ -11,6 +22,8 @@ import * as React from "react";
 import { AtsConfigPreview } from "./AtsConfigPreview";
 import { RefappAtsConfig } from "./lib/ats-types";
 import "./style.css";
+import { z } from "zod";
+import { fromPairs, map, random } from "lodash";
 
 /**
  * From the config-examples directory in this repository
@@ -24,6 +37,12 @@ const configFiles = [
   "Project Templates, existing project, Swedish.json",
   "Project Templates.json",
 ] as const;
+type ConfigFile = (typeof configFiles)[number];
+const toConfigFile = (value: unknown) =>
+  z
+    .enum(["", ...configFiles])
+    .catch("")
+    .parse(value);
 
 const createOurTheme = (): Theme => {
   return createTheme({
@@ -34,62 +53,308 @@ const createOurTheme = (): Theme => {
       h3: { fontSize: 18 },
       h4: { fontSize: 16 },
     },
+    palette: {
+      primary: {
+        main: "#fd954b",
+      },
+      background: {
+        default: "#b16834",
+      },
+    },
   });
 };
 
-export default function App() {
-  const [atsConfigFile, setAtsConfigFile] = React.useState<string>();
-  const [atsConfig, setAtsConfig] = React.useState<RefappAtsConfig>();
-  const theme = React.useMemo(() => createOurTheme(), []);
+const configMethods = ["sample", "live"] as const;
+const defaultConfigMethod = configMethods[0];
+type ConfigMethod = (typeof configMethods)[number];
+const toConfigMethod = (value: unknown) =>
+  z.enum(configMethods).catch(defaultConfigMethod).parse(value);
 
-  React.useEffect(() => {
-    if (!atsConfigFile) {
-      return;
-    }
-    fetch(
+const safeUrl = (url: string) => {
+  try {
+    return new URL(url);
+  } catch (_) {
+    return undefined;
+  }
+};
+
+const createConfigError = (message: string): RefappAtsConfig => ({
+  config: {
+    fields: [
+      {
+        id: "error",
+        type: "text",
+        label: message ?? "An error occurred",
+      },
+    ],
+  },
+});
+
+const fetchFromGitHub = async (
+  atsConfigFile: ConfigFile
+): Promise<RefappAtsConfig> => {
+  try {
+    const response = await fetch(
       `https://raw.githubusercontent.com/ref-app/refapp-api/main/config-examples/${encodeURIComponent(
         atsConfigFile
       )}`
-    ).then(
-      (response) => {
-        response.json().then((config) => {
-          setAtsConfig(config);
-        });
-      },
-      (err: any) =>
-        setAtsConfig({
-          config: {
-            fields: [
-              {
-                id: "error",
-                type: "text",
-                label: err.message ?? "An error occurred",
-              },
-            ],
-          },
-        })
     );
-  }, [atsConfigFile]);
+    const config = await response.json();
+    return config;
+  } catch (e) {
+    return createConfigError(e.message);
+  }
+};
+
+const fetchFromRefapp = async (
+  configEndpoint: string,
+  atsSecret: string,
+  customerSecret: string
+): Promise<RefappAtsConfig> => {
+  try {
+    const response = await fetch(configEndpoint, {
+      headers: {
+        "Authorization": `Bearer ${atsSecret}`,
+        "X-Provider-Key": customerSecret,
+      },
+    });
+    const config = await response.json();
+    if (!response.ok) {
+      return createConfigError(config.message);
+    }
+    console.info("Config from Refapp", config);
+    return config;
+  } catch (e) {
+    return createConfigError(e.message);
+  }
+};
+
+const generateCandidate = (recruiterDomain: string) => {
+  const candidateId = Math.random().toString(36).slice(2);
+  const jobId = Math.random().toString(36).slice(2);
+  return {
+    "id": candidateId,
+    "first-name": "John",
+    "last-name": "Doe",
+    "email": `john.doe.${candidateId}@private.example`,
+    "phone": `+467017406${random(0, 94) + 5}`,
+    "language": "en",
+    "job": {
+      id: jobId,
+      title: `Job #${jobId}`,
+    },
+    "recruiter": {
+      "first-name": "Richard",
+      "last-name": "Roe",
+      "email": `richard.roe@${recruiterDomain}`,
+    },
+  };
+};
+
+const submitLive = async (
+  postEndpoint: string,
+  atsSecret: string,
+  customerSecret: string,
+  customerDomain: string,
+  dataObject: Record<string, any>
+): Promise<string> => {
+  try {
+    const response = await fetch(postEndpoint, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${atsSecret}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "partner-event": {
+          "company": { uuid: customerSecret },
+          "candidate": generateCandidate(customerDomain),
+          "partner-result": {},
+          "webhook-data": dataObject,
+        },
+      }),
+    });
+    const json = await response.json();
+    return JSON.stringify(json, undefined, 2);
+  } catch (e) {
+    return e.message;
+  }
+};
+
+export default function App() {
+  const [configMethod, setConfigMethod] =
+    React.useState<ConfigMethod>(defaultConfigMethod);
+  const [atsConfigFile, setAtsConfigFile] = React.useState<ConfigFile | "">("");
+  const [atsConfig, setAtsConfig] = React.useState<RefappAtsConfig>();
+  const [liveConfigEndpoint, setLiveConfigEndpoint] =
+    React.useState<string>("");
+  const [livePostEndpoint, setLivePostEndpoint] = React.useState<string>("");
+  const [liveAtsSecret, setLiveAtsSecret] = React.useState<string>("");
+  const [liveCustomerSecret, setLiveCustomerSecret] =
+    React.useState<string>("");
+  const [liveCustomerDomain, setLiveCustomerDomain] =
+    React.useState<string>("");
+  const [submitResults, setSubmitResults] = React.useState<string>("");
+  const [resetTrigger, setResetTrigger] = React.useState<number>(0);
+  const theme = React.useMemo(() => createOurTheme(), []);
+
+  React.useEffect(() => {
+    const configEndpointUrl = safeUrl(liveConfigEndpoint);
+    const postEndpointUrl = safeUrl(livePostEndpoint);
+    if (configMethod === "sample" && atsConfigFile !== "") {
+      fetchFromGitHub(atsConfigFile).then((atsConfig) =>
+        setAtsConfig(atsConfig)
+      );
+    } else if (
+      configMethod === "live" &&
+      configEndpointUrl !== undefined &&
+      configEndpointUrl.pathname.length > 0 &&
+      postEndpointUrl !== undefined &&
+      postEndpointUrl.pathname.length > 0 &&
+      liveAtsSecret.length > 0 &&
+      liveCustomerSecret.length > 0
+    ) {
+      fetchFromRefapp(
+        liveConfigEndpoint,
+        liveAtsSecret,
+        liveCustomerSecret
+      ).then((atsConfig) => setAtsConfig(atsConfig));
+    } else {
+      setAtsConfig(undefined);
+    }
+  }, [
+    configMethod,
+    atsConfigFile,
+    liveConfigEndpoint,
+    liveAtsSecret,
+    liveCustomerSecret,
+    resetTrigger,
+  ]);
+
+  const handleChange = (event: React.SyntheticEvent) => {
+    setConfigMethod(
+      toConfigMethod(
+        event.currentTarget.closest("[id^='config-']").id.substring(7)
+      )
+    );
+  };
+
+  const handleSubmit = (formData: FormData) => {
+    const dataObject = fromPairs(Array.from(formData));
+    submitLive(
+      livePostEndpoint,
+      liveAtsSecret,
+      liveCustomerSecret,
+      liveCustomerDomain,
+      dataObject
+    ).then((result) => setSubmitResults(result));
+  };
 
   return (
     <ThemeProvider theme={theme}>
-      <Box display="flex" flexDirection="column" gap={2}>
-        <Typography variant="h1">Refapp configuration UI sample</Typography>
-        <Select
-          value={atsConfigFile}
-          onChange={(e) => setAtsConfigFile(e.target.value)}
-        >
-          {configFiles.map((f) => (
-            <MenuItem key={f} value={f}>
-              {f}
-            </MenuItem>
-          ))}
-        </Select>
+      <CssBaseline />
+      <Container sx={{ display: "flex", flexDirection: "column", my: 2 }}>
+        {/* All Accordions must share a common parent for the final rounded corners to work. */}
+        <Box>
+          <Accordion
+            id="config-sample"
+            expanded={configMethod === "sample"}
+            onChange={handleChange}
+            disableGutters
+          >
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography variant="h1">
+                Refapp configuration UI sample
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Select
+                value={atsConfigFile}
+                onChange={(e) => setAtsConfigFile(toConfigFile(e.target.value))}
+                sx={{
+                  width: 1,
+                }}
+              >
+                {configFiles.map((f) => (
+                  <MenuItem key={f} value={f}>
+                    {f}
+                  </MenuItem>
+                ))}
+              </Select>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion
+            id="config-live"
+            expanded={configMethod === "live"}
+            onChange={handleChange}
+            disableGutters
+          >
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography variant="h1">Live Refapp test</Typography>
+            </AccordionSummary>
+            <AccordionDetails
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+              }}
+            >
+              <TextField
+                label={"Configuration Endpoint"}
+                value={liveConfigEndpoint}
+                onChange={(e) => setLiveConfigEndpoint(e.currentTarget.value)}
+              />
+              <TextField
+                label={"POST Endpoint"}
+                value={livePostEndpoint}
+                onChange={(e) => setLivePostEndpoint(e.currentTarget.value)}
+              />
+              <TextField
+                label={"ATS secret"}
+                value={liveAtsSecret}
+                onChange={(e) => setLiveAtsSecret(e.currentTarget.value)}
+              />
+              <TextField
+                label={"Customer secret"}
+                value={liveCustomerSecret}
+                onChange={(e) => setLiveCustomerSecret(e.currentTarget.value)}
+              />
+              <TextField
+                label={"Customer email domain"}
+                value={liveCustomerDomain}
+                onChange={(e) => setLiveCustomerDomain(e.currentTarget.value)}
+              />
+            </AccordionDetails>
+          </Accordion>
+        </Box>
 
         {atsConfig && (
-          <AtsConfigPreview configFields={atsConfig.config.fields} />
+          <Card sx={{ mt: 2 }}>
+            <CardContent>
+              {configMethod === "live" ? (
+                <AtsConfigPreview
+                  configFields={atsConfig.config.fields}
+                  onReset={() => setResetTrigger((count) => count + 1)}
+                  onSubmit={handleSubmit}
+                />
+              ) : (
+                <AtsConfigPreview
+                  configFields={atsConfig.config.fields}
+                  onReset={() => setResetTrigger((count) => count + 1)}
+                />
+              )}
+            </CardContent>
+          </Card>
         )}
-      </Box>
+
+        {submitResults && (
+          <Card sx={{ mt: 2}}>
+            <Box sx={{whiteSpace: "pre", m: 2, p: 1, bgcolor: "grey.50", borderColor: "grey.300", borderWidth: 1, borderStyle: "solid", fontFamily: "monospace"}}>
+              {submitResults}
+            </Box>
+          </Card>
+        )}
+      </Container>
     </ThemeProvider>
   );
 }
--- react-sample/src/AtsConfigPreview.tsx
+++ react-sample/src/AtsConfigPreview.tsx
@@ -2,6 +2,7 @@ import {
   Alert,
   AlertProps,
   Box,
+  Button,
   Checkbox,
   FormControl,
   FormControlLabel,
@@ -68,13 +69,19 @@ export const AtsConfigFieldPreview = ({
     field.value
   );
 
+  React.useEffect(() => {
+    setValue(field.value);
+  }, [field]);
+
   switch (field.type) {
     case "checkbox":
       return (
         <FormControlLabel
+          sx={{ mx: 1, minWidth: 120 }}
           label={field.label}
           control={
             <Checkbox
+              name={field.id}
               disabled={field.disabled}
               checked={_.isBoolean(value) ? value : false}
               onChange={(e) => setValue(e.target.checked)}
@@ -105,6 +112,7 @@ export const AtsConfigFieldPreview = ({
         <FormControl sx={{ m: 1, minWidth: 120 }}>
           <InputLabel id={field.id}>{field.label}</InputLabel>
           <Select
+            name={field.id}
             label={field.label}
             value={_.isString(value) ? value : ""}
             disabled={field.disabled}
@@ -122,6 +130,8 @@ export const AtsConfigFieldPreview = ({
     case "text":
       return (
         <TextField
+          name={field.id}
+          sx={{ m: 1, minWidth: 120 }}
           disabled={field.disabled}
           label={field.label}
           value={_.isString(value) ? value : ""}
@@ -135,13 +145,39 @@ export const AtsConfigFieldPreview = ({
 
 type AtsConfigPreviewProps = Readonly<{
   configFields: ReadonlyArray<AtsConfigField>;
+  onReset: () => void;
+  onSubmit?: (data: FormData) => void;
 }>;
-export const AtsConfigPreview = ({ configFields }: AtsConfigPreviewProps) => {
+export const AtsConfigPreview = ({
+  configFields,
+  onReset,
+  onSubmit,
+}: AtsConfigPreviewProps) => {
   return (
-    <Box display="flex" flexDirection="column" gap={2}>
+    <Box
+      display="flex"
+      flexDirection="column"
+      gap={2}
+      mb={0}
+      component={"form"}
+    >
       {configFields.map((field) => (
         <AtsConfigFieldPreview key={field.id} field={field} />
       ))}
+      <Box display="flex" gap={2} mx={1}>
+        {onSubmit && (
+          <Button
+            variant="contained"
+            sx={{ flexGrow: 1 }}
+            onClick={() => onSubmit(new FormData(document.forms[0]))}
+          >
+            Submit
+          </Button>
+        )}
+        <Button sx={{ flexGrow: 1 }} onClick={() => onReset()}>
+          Reset
+        </Button>
+      </Box>
     </Box>
   );
 };
